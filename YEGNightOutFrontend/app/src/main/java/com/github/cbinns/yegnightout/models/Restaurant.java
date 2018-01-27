01aae17c2235c08a690770874d54b73916381702
cbinns <cbinns@ualberta.ca>
2018-01-27 15:31:05 -0700

    :seenoevil: classes for rest & spec

--- YEGNightOutFrontend/app/src/main/java/com/github/cbinns/yegnightout/models/Restaurant.java
+++ YEGNightOutFrontend/app/src/main/java/com/github/cbinns/yegnightout/models/Restaurant.java
@@ -1,5 +1,7 @@
 package com.github.cbinns.yegnightout.models;
 
+import java.util.ArrayList;
+
 /**
  * Created by Abinsi on 1/27/2018.
  */
@@ -8,18 +10,20 @@ public class Restaurant {
     private int id;
     private String name;
     private String address;
+    private ArrayList<Special> specials;
 
-    public Restaurant(int id, String name, String address){
+    public Restaurant(int id, String name, String address, ArrayList<Special> specials){
         this.id=id;
         this.name=name;
         this.address=address;
+        this.specials=specials;
    }
 
    public Restaurant(){
         this.id=-1;
         this.name="";
         this.address="";
-   }x
+   }
 
    public void setName(String name){
        this.name=name;
@@ -33,15 +37,22 @@ public class Restaurant {
        this.address=address;
    }
 
-    public int getId() {
-        return id;
-    }
+   public void setSpecials(ArrayList<Special> specials) {
+       this.specials = specials;
+   }
 
-    public String getName() {
-        return name;
-    }
+   public int getId() {
+       return id;
+   }
+   public String getName() {
+       return name;
+   }
 
-    public String getAddress() {
-        return address;
+   public String getAddress() {
+       return address;
+   }
+
+    public ArrayList<Special> getSpecials() {
+        return specials;
     }
 }
